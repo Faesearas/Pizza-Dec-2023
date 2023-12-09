d9f4389b3f8e60bf07221de252420e2407332954
AmyHruska <amyhruskacda@gmail.com>
2023-12-09 14:16:44 -0800

    add price method to pizza

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,9 @@ function Pizza(toppings, size){
     this.size = size;
 }
 
+Pizza.prototype.price = function () {
+    let price = this.size * 5;
+    for (let i=0; i <= this.toppings.length - 1; i++)
+    price += 1;
+    this.price = price;
+};
